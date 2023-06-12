4d43f0a4ddec79499dcb166e0b559a88933741fb
Oleksandr Chervonniy <sheph@ukr.net>
2023-06-12 21:05:13 +0200

    Currency convert task added

--- main.js
+++ main.js
@@ -194,3 +194,28 @@ class Collapser {
 
 new Collapser('collapsible');
 new Collapser('collapsible2');
+
+function convert(sum, initialCurrency, convertCurrency) {
+	const allCurrencies = [
+		{ name: 'USD', mult: 1 },
+		{ name: 'UAH', mult: 1 / 37 },
+		{ name: 'EUR', mult: 1.1 },
+	];
+
+	const initial = allCurrencies.find((c) => c.name === initialCurrency);
+	if (!initial) {
+		return null;
+	}
+
+	const convert = allCurrencies.find((c) => c.name === convertCurrency);
+	if (!convert) {
+		return null;
+	}
+
+	return new Intl.NumberFormat('ua-Ua', {
+		style: 'currency',
+		currency: convert.name,
+	}).format((sum * initial.mult) / convert.mult);
+}
+
+console.log(convert(2500, 'USD', 'EUR'));
